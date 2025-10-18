7651b3c5a5ec6d09c22e0f956509bef3863e32fc
Stiven Tovar <stivent367@gmail.com>
2025-10-18 18:48:25 -0500

    feat: Add Swagger descriptions

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -68,7 +68,9 @@ app.MapGet("/api/properties", async (
 
 })
 .WithName("GetProperties")
-.WithTags("Properties");
+.WithTags("Properties")
+.WithSummary("Obtener lista de propiedades (con filtros)")
+.WithDescription("Devuelve una lista de propiedades. Si no se envían parámetros de consulta, devuelve todas. Se puede filtrar por 'name', 'address', 'minPrice' y 'maxPrice'.");
 
 app.MapGet("/api/properties/{id}", async (string id, IPropertyRepository propertyRepo) =>
 {
@@ -83,6 +85,8 @@ app.MapGet("/api/properties/{id}", async (string id, IPropertyRepository propert
 
 })
 .WithName("GetPropertyById")
-.WithTags("Properties");
+.WithTags("Properties")
+.WithSummary("Obtener detalles de una propiedad por su ID")
+.WithDescription("Devuelve los detalles completos de una propiedad específica usando su ID (ObjectId).");
 
 app.Run();
